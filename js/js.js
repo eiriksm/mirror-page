37ed7ce6a3c9fbac7431082053f0a596c124348c
Eirik S. Morland <eirik@morland.no>
2016-01-07 21:11:01 +0100

    Lousy old android crap phone crap.

--- js/js.js
+++ js/js.js
@@ -7,23 +7,27 @@ var cl = document.getElementById('clock');
 setInterval(function() {
   cl.innerHTML = m().format('ddd DD.MM HH:mm:ss')
 }, 1000)
-request.get(url)
-  .end(function(e, res) {
-    if (e) {
-      alert(e);
-    }
-    var bod = res.text;
-    // Get rid of that xml.
-    parseString(bod, function(err, result) {
-      result.weatherdata.forecast[0].tabular[0].time.forEach(function(n, i) {
-        if (i > 5) {
-          return;
-        }
-        var date = m(n['$'].from);
-        el.innerHTML += '<div class="weatherline">' + 
-          '<div class="temp">' + n.temperature[0]['$'].value + '</div>' +
-          '<div class="date">' + date.format('DD.MM HH:mm') + '</div>' +
-          '</div>';
+function tryToFetchSomething() {
+  request.get(url)
+    .end(function(e, res) {
+      if (e) {
+        alert(e);
+      }
+      var bod = res.text;
+      // Get rid of that xml.
+      parseString(bod, function(err, result) {
+        result.weatherdata.forecast[0].tabular[0].time.forEach(function(n, i) {
+          if (i > 5) {
+            return;
+          }
+          var date = m(n['$'].from);
+          el.innerHTML += '<div class="weatherline">' + 
+            '<div class="temp">' + n.temperature[0]['$'].value + '</div>' +
+            '<div class="date">' + date.format('DD.MM HH:mm') + '</div>' +
+            '</div>';
+        })
       })
-    })
-})
+  })
+}
+setTimeout(tryToFetchSomething, 2000);
+setTimeout(tryToFetchSomething, 10000);
